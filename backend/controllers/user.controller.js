f355bd64955ecb2df07a60cb7769ee1a4731201e
koteswara <koteswara.sampathi@gmail.com>
2026-09-16 08:38:15 +0530

    Fix production authentication cookie

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -87,8 +87,8 @@ export const login = async (req, res) => {
 
         res.cookie("token", token, {
             httpOnly: true,
-            secure: process.env.NODE_ENV === "production",
-            sameSite: "lax",
+            secure: true,
+            sameSite: "none",
             maxAge: 24 * 60 * 60 * 1000
         });
 
